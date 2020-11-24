051b0014ab87e07890c42fd51643396995ca09af
Katie Strong <katiestrong123@gmail.com>
2020-11-24 16:18:37 +0000

    task one

--- console.py
+++ console.py
@@ -1,31 +1,13 @@
 import pdb
 from models.artist import Artist
-import repositories.user_repository as artist_repository
+import repositories.artist_repository as artist_repository
 
 
-# from models.task import Task
-# from models.user import User
-# import repositories.task_repository as task_repository
 
-
-task_repository.delete_all()
-user_repository.delete_all()
-
-Artist1 = User('Depeche Mode')
+Artist1 = Artist('Depeche Mode')
 artist_repository.save(Artist1)
 
-Artist2 = User('Metallica')
+Artist2 = Artist('Metallica')
 artist_repository.save(Artist2)
-# jack = User('Jack', 'Jarvis')
-# user_repository.save(jack)
-
-# victor = User('Victor', 'McDade')
-# user_repository.save(victor)
-
-# dishes = Task("So the dishes", jack, 4)
-# task_repository.save(dishes)
-
-# homework = Task("Do the homework", jack, 5)
-# task_repository.save(homework)
 
 pdb.set_trace()
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -2,8 +2,9 @@ from db.run_sql import run_sql
 from models.artist import Artist 
 
 def save(artist):
-    sql = "INSERT INTO artist (name) VALUES %s RETURNING *"
+    sql = "INSERT INTO artists (name) VALUES (%s) RETURNING *"
     values = [artist.name]
     results = run_sql(sql, values)
+    id = results[0]['id']
     artist.id = id
     return artist
